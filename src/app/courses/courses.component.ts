bfeec8f77146828b6d38f0c97c251598c96c3a8d
Tjejmie <jamie_svanberg@hotmail.com>
2023-01-10 12:02:57 +0100

    possible to see all miun-courses

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'; // to get data from the route used to show this component
+import { BackendService } from '../backend.service'; // this component needs our backend service
+import { courses } from '../courses.model'; // this component needs our User interface (model)
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css']
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
 
+ courses: courses[];
+  
+ constructor(private backend: BackendService, private route: Router) {
+
+   this.courses = [];
+ }
+
+ gotoHome(){
+  this.route.navigate(['/my-courses']);  // define your component where you want to go
+}
+
+ ngOnInit(): void {
+   this.getCourses();
+ }
+
+
+ getCourses() {
+
+   this.backend.getCourses()
+     .then(courses => {
+
+       this.courses = courses;
+     })
+     .catch(error => console.error(`An error occurred getting all users: ${error}`));
+  }
 }
--- src/app/my-courses/my-courses.component.ts
+++ src/app/my-courses/my-courses.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'; // to get data from the route used to show this component
+import { BackendService } from '../backend.service'; // this component needs our backend service
+import { myCourses } from '../my-course.model'; // this component needs our User interface (model)
 
 @Component({
   selector: 'app-my-courses',
@@ -7,4 +10,29 @@ import { Component } from '@angular/core';
 })
 export class MyCoursesComponent {
 
+    myCourses: myCourses[];
+ 
+    constructor(private backend: BackendService, private route: ActivatedRoute) {
+
+      this.myCourses = []; // initially empty
+
+    }
+  
+    ngOnInit(): void {
+      this.getMyCourses();
+  
+    }
+  
+    getMyCourses() {
+
+      this.backend.getMyCourses()
+        .then(myCourses => {
+
+          this.myCourses = myCourses;
+  
+
+        })
+        .catch(error => console.error(`An error occurred getting all users: ${error}`));
+    
+  }
 }
